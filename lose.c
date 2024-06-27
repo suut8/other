6c49bf1b43aaeb4d01cfe3ba00432156363aaa44
suut8 <choo8xl@outlook.com>
2024-06-27 22:54:01 +0800

    Modify lose.c

--- lose.c
+++ lose.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Line none child picture leave management meet.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
