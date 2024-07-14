6cd6872a3a5fce0b861a71cd54508f51472c87e1
suut8 <choo8xl@outlook.com>
2024-07-14 15:12:25 +0800

    Modify stay.c

--- stay.c
+++ stay.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Resource carry card citizen simply under.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
