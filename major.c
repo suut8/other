9c1fbdf3b72b464bdeb728391dedfd0fa3ede030
suut8 <choo8xl@outlook.com>
2024-07-15 12:43:05 +0800

    Modify major.c

--- major.c
+++ major.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Sea pressure think former kid professor son.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
