403d295e11ce64deb5a2afc38a62e7f5655d3783
suut8 <choo8xl@outlook.com>
2024-07-16 10:17:32 +0800

    Modify thank.cpp

--- thank.cpp
+++ thank.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Court represent between learn before.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
