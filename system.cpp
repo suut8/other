155be59b6b8a2f1aaf887f14073b06da391ea0ab
suut8 <choo8xl@outlook.com>
2024-08-05 22:58:30 +0800

    Modify system.cpp

--- system.cpp
+++ system.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Threat school argue.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
