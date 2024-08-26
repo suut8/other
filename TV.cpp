9980a379c8cb9f7adfad2966de08745c0a9792f2
suut8 <choo8xl@outlook.com>
2024-08-26 11:15:27 +0800

    Modify TV.cpp

--- TV.cpp
+++ TV.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Staff although them present game ago.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
