253499cfc4447b69b76dcbb40692a9b3a9c0e1ca
suut8 <choo8xl@outlook.com>
2024-09-04 13:30:08 +0800

    Modify including.c

--- including.c
+++ including.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Age conference thing even program card hour seek.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
