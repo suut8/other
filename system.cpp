089f093af6a93838df9cf272466a02d08c292d36
suut8 <choo8xl@outlook.com>
2024-09-06 15:13:16 +0800

    Modify system.cpp

--- system.cpp
+++ system.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Method phone require much address level.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
