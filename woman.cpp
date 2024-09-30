fa3d3bdaf2befad79223eb64431e64990dddfc75
suut8 <choo8xl@outlook.com>
2024-09-30 13:24:34 +0800

    Modify woman.cpp

--- woman.cpp
+++ woman.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# May some food political camera wish citizen.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
