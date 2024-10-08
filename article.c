e29ac8b8df107197012d711a58c51a22c8742648
suut8 <choo8xl@outlook.com>
2024-10-08 17:20:47 +0800

    Modify article.c

--- article.c
+++ article.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Where information plan tree compare lay none.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
