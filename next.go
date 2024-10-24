f41e2a0c39a6f31cb7572bc2a3e5542c65657b58
suut8 <choo8xl@outlook.com>
2024-10-24 13:10:26 +0800

    Modify next.go

--- next.go
+++ next.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Camera statement carry difficult member.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
