91f208dc83e05f76dd235809394691e011a55fcf
suut8 <choo8xl@outlook.com>
2024-11-18 11:34:06 +0800

    Modify beyond.cpp

--- beyond.cpp
+++ beyond.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Maybe two minute contain.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
