88e607743521cb8d9a447baf036dd8e65c67f9b6
suut8 <choo8xl@outlook.com>
2025-02-07 23:38:45 +0800

    Modify organization.c

--- organization.c
+++ organization.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Should kind first name size modern throughout.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
