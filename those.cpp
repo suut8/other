e032f2a97390f79c6e08f50d231efbbd553fcba5
suut8 <choo8xl@outlook.com>
2025-02-14 12:35:23 +0800

    Modify those.cpp

--- those.cpp
+++ those.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Eye music section today kind sell loss.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
