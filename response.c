b52d75acdffd80e37ae70b59e8c4e15764a67b6c
suut8 <choo8xl@outlook.com>
2025-02-19 21:00:38 +0800

    Modify response.c

--- response.c
+++ response.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Reveal account among bad else seem fact late.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
